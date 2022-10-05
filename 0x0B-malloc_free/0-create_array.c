0dee30af6695c52e1d89a27842b7f519a896c913
Chizitere <chizzit45@gmail.com>
2022-10-04 17:19:22 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,9 @@
 /**
- * create_array -> creating array
- * @size: size of array to be created
- * @c: character an array is initialized with
- * Return: a pointer to an array
- */
+* create_array -> creating array
+* @size: size of array to be created
+* @c: character an array is initialized with
+* Return: a pointer to an array
+*/
 
 char *create_array(unsigned int size, char c)
 {
